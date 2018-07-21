f4a32645335dcaaa7cfa52a891e98e4d74046b6a
chengguruchun <1725788349@qq.com>
2018-07-21 20:38:51 +0800

    new branch

---
+++ src/newbranch/demo.java
@@ -0,0 +1,14 @@
+package newbranch;
+
+/**
+ * @Author :cheng
+ * @Description:
+ * @Date: created in 20:34 2018/7/21
+ * @Reference:
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("new branch!");
+
+    }
+}
